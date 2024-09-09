ac6449471f5f65cc625fa8104cc55c0b5133b8c5
Idorngetsit <udohamos02@gmail.com>
2024-09-09 03:46:25 -0700

    work

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-// window  
+
+// for header 
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+// for window scroll
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  }
+}
+
+window.onload = () =>{
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add('active');
+  }else{
+    document.querySelector('.header').classList.remove('active');
+  }
+}
   
-  
-  
-  
-  
-  
-  
-  
+// for home section
+
   var swiper = new Swiper(".home-slider", {
       spaceBetween: 20,
       effect: "fade",
@@ -41,5 +66,8 @@
             820: {
                 slidesPerView: 3,
             },
+            // 1100: {
+            //     slidesPerView: 3,
+            // }
         },
       });
